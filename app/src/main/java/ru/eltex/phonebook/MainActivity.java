a3910e9a9748d394addba59ea602da19e3e408b5
KarasiK <polkavod@gmail.com>
2022-11-10 23:38:47 +0700

    add phone list

--- app/src/main/java/ru/eltex/phonebook/MainActivity.java
+++ app/src/main/java/ru/eltex/phonebook/MainActivity.java
@@ -3,6 +3,7 @@ package ru.eltex.phonebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.io.IOException;
 
@@ -14,28 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User user = new User("Иван", "Иванов", "Иванович",
-                "8800553535","ул. Гагарина, д. 1, кв. 19");
-        try {
-            user.save(FILE_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ListView mainList = (ListView) findViewById(R.id.main_list);
 
-        try {
-            user.load(FILE_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        User[] users = {
+                new Programmer("Vadim", "Chernyavsky", "Vasilyevich", "8800553535",
+                        "Gagarina 1", "Junior Android Developer"),
+                new Programmer("Konstantin", "Konovalov", "Olegovich", "89129003468",
+                        "Lenina 10", "Student"),
+                new Programmer("Egor", "Fokin", "Konstantinovich", "89139993456",
+                        "Bluhera 32/1", "Senior C++ Backend Developer"),
+                new Programmer("Maxim", "Vasilchenko", "Aleksandrovich", "89135436712",
+                        "Bluhera 32/1","Junior Embedded C Developer"),
+                new Programmer("Roman", "Shevlyakov", "Valerievich", "89132563245",
+                        "Titova 13", "Junior Embedded C Developer"),
+                new Manager("Vladislav", "Sizov", "Aleksandrovich", "89140983245",
+                        "Koroleva 4", "v.sizov123@mail.ru"),
+                new Manager("Ivan", "Ivanov", "Ivanovich", "89003456789",
+                        "Lenina 3", "i.ivanov123@gmail.com"),
+                new Manager("Anatoly", "Buravov", "Sergeevich", "89140072258",
+                        "Nemirovich-Danchenko 124", "a.buravov@yandex.ru")
+        };
 
-        User nextUser = null;
-
-        try {
-            nextUser.load(FILE_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        PhoneAdapter phoneAdapter = new PhoneAdapter(this, users);
+        mainList.setAdapter(phoneAdapter);
 
     }
 }
--- app/src/main/java/ru/eltex/phonebook/Programmer.java
+++ app/src/main/java/ru/eltex/phonebook/Programmer.java
@@ -2,22 +2,12 @@ package ru.eltex.phonebook;
 
 public class Programmer extends User{
 
-    private String position;
     private String specialization;
 
     public Programmer(String name, String surname, String patronymic,
-                      String phoneNumber, String address, String position, String specialization) {
+                      String phoneNumber, String address, String specialization) {
         super(name, surname, patronymic, phoneNumber, address);
-        this.position = position;
         this.specialization = specialization;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
     }
 
     public String getSpecialization() {
